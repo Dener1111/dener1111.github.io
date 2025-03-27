7473f8e14fc6850864bdcf414af6861a37644f26
Dener1111 <mr.dener1111@gmail.com>
2025-03-27 17:43:16 +0100

    added bg

--- script.js
+++ script.js
@@ -115,3 +115,8 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener('mousemove', (e) => {
+    document.documentElement.style.setProperty('--cursor-x', e.clientX + 'px');
+    document.documentElement.style.setProperty('--cursor-y', e.clientY + 'px');
+});
